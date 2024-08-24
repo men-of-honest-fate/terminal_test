7c4d3f7d7765d323eec00e6853f0f8cec97d844a
SofyaFin <sonyafinutina@gmail.com>
2024-08-24 22:39:50 +0300

    ujbyjhn

--- api/models/db.py
+++ api/models/db.py
@@ -2,19 +2,18 @@ import sqlalchemy as sa
 from sqlalchemy import Column
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column
+from .base import Base
 # site:
 # id - int
 # url - str
 # bank_number: str
 # login: str
 # password: str
-class Base(declarative_base):
-    pass
 class Site(Base):
     __tablename__ = "site"
     id: Mapped[int] = mapped_column(primary_key = True)
     url: Mapped[str] = mapped_column(nullable = False)
-    bank_number: Mapped[str] = mapped_column(nulable = False)
+    bank_number: Mapped[str] = mapped_column(nullable = False)
     login: Mapped[str] = mapped_column(nullable = False)
     password: Mapped[str] = mapped_column(nullable = False)
 
