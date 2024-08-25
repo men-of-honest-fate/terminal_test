352a226a37233fd4dd6da91e9eec4eafd2606f51
SofyaFin <sonyafinutina@gmail.com>
2024-08-25 18:53:40 +0300

    ghcvjg

--- api/routes/sites.py
+++ api/routes/sites.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models.db import Site
 import models
-import models.crud
+from models.crud import create_site, create_terminal, get_site, get_terminal
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
@@ -28,35 +28,23 @@ def get_db():
 
 
 @app.post("/create_site/", response_model=Create_Site)
-def create_site(site: Create_Site, db: Session = Depends(get_db)):
-    db_site = crud.create_site(db)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+def create_site1(site: Create_Site, db: Session = Depends(get_db)):
+    db_site = create_site(db)
+    if db_site:
+        raise HTTPException(status_code=400, detail="Site happily created")
+    return create_site(db=db, site=site)
+#post запрос инфа нам, get пользователям 
+@app.post("/create_terminal/", response_model=Create_Terminal)
+def create_terminal1(terminal: Create_Terminal, db: Session = Depends(get_db)):
+    db_terminal = create_terminal(db)
+    if db_terminal:
+        raise HTTPException(status_code=400, detail="Terminal happily created")
+    return create_terminal(db=db, terminal=terminal)
+@app.post("/get_site/", response_model=Get_Site)
+def get_site1(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    site = get_site(db, skip=skip, limit=limit)
+    return site
+@app.post("/get_terminal/", response_model=Get_Site)
+def get_terminal1(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    terminal = get_terminal(db, skip=skip, limit=limit)
+    return terminal
