bff0b8c7661311a315b8ce339548feac803f2f1d
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-09-17 23:03:39 +0300

    Create base.py

---
+++ api/models/create_site.py/base.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>QR Код для оплаты</title>
+</head>
+<body>
+    <h1>QR Код для оплаты</h1>
+    <img src="{qr_code}" alt="QR Code for Payment">
+</body>
+</html>
